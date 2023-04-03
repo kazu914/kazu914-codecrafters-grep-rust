68fce5eb3ca1b6d60c8a729dcdd9936c157385e7
kazu914 <nkz0914ssk@gmail.com>
2023-04-03 18:38:57 +0900

    #6

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,64 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
     }
 }
 
+fn compile_patterns(patterns_input: &str) -> Vec<String> {
+    let mut patterns: Vec<String> = vec![];
+
+    let mut is_special = false;
+    let mut is_selector = false;
+    let mut selector = "".to_string();
+
+    for (i, c) in patterns_input.chars().enumerate() {
+        if is_selector {
+            if let ']' = c {
+                patterns.push(format!("[{}]", selector));
+                is_selector = false
+            } else {
+                selector = format!("{}{}", selector, c);
+            }
+            continue;
+        }
+        if is_special {
+            match c {
+                'd' => patterns.push(r"\d".to_string()),
+                'w' => patterns.push(r"\w".to_string()),
+                '\\' => patterns.push(r"\".to_string()),
+                _ => panic!("Unknown Special Charactor!"),
+            }
+            is_special = false;
+            continue;
+        }
+        match c {
+            '\\' => is_special = true,
+            '[' => is_selector = true,
+            _ => patterns.push(patterns_input[i..i + 1].to_string()),
+        }
+    }
+
+    patterns
+}
+
+fn match_here(input_line: &str, patterns: Vec<String>) -> bool {
+    if patterns.is_empty() {
+        return true;
+    }
+
+    if input_line.is_empty() {
+        return false;
+    }
+
+    let res = match_pattern(
+        &input_line.chars().next().unwrap().to_string(),
+        patterns.first().unwrap(),
+    );
+
+    if res {
+        match_here(&input_line[1..], patterns[1..].to_vec())
+    } else {
+        match_here(&input_line[1..], patterns)
+    }
+}
+
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -51,7 +109,9 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    if match_pattern(&input_line, &pattern) {
+    let patterns = compile_patterns(&pattern);
+
+    if match_here(&input_line, patterns) {
         process::exit(0)
     } else {
         process::exit(1)
